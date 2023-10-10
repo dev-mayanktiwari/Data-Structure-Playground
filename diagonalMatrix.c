787c75ef24a48e86ebd7e866125c62bf9186f305
Mayank <mayanktiwari1212.mt@gmail.com>
2023-10-10 21:37:42 +0530

    diagonalMatrix-Updated

--- diagonalMatrix.c
+++ diagonalMatrix.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 
 struct matrix {
     int A[10];
@@ -7,25 +7,25 @@ struct matrix {
 };
 
 void set(struct matrix *m, int i, int j, int x) {
-    if (i==j) {
-        m->A[i-1]=x;
+    if (i == j && i >= 1 && i <= m->n) {
+        m->A[i - 1] = x;
     }
 }
 
 void get(struct matrix m, int i, int j) {
-    if (i==j) {
-        printf("%d ", m.A[i-1]);
+    if (i == j && i >= 1 && i <= m.n) {
+        printf("%d ", m.A[i - 1]);
     }
     else {
         printf("0 ");
     }
 }
 
-void display (struct matrix m, int i, int j) {
-    for (int i=0; i<m.n; i++) {
-        for (int j=0; j<m.n; j++) {
-            if (i==j) {
-                printf("%d ", m.A[i-1]);
+void display(struct matrix m) {
+    for (int i = 1; i <= m.n; i++) {
+        for (int j = 1; j <= m.n; j++) {
+            if (i == j) {
+                printf("%d ", m.A[i - 1]);
             }
             else {
                 printf("0 ");
@@ -36,25 +36,26 @@ void display (struct matrix m, int i, int j) {
 }
 
 int main() {
-    struct matrix m;   
+    struct matrix m;
     int n;
-    printf("Enter the dimension of the matrix: ");
-    if (n<0 || n>10) {
+    printf("Enter the dimension of the matrix: \n");
+    scanf("%d", &n);
+    if (n < 0 || n > 10) {
         printf("Invalid dimension.\n");
         return 1;
     }
     else {
-        m.n=n;
+        m.n = n;
     }
     printf("Enter the diagonal elements: \n");
-    for (int i=1;i<=n;i++) {
+    for (int i = 1; i <= n; i++) {
         int x;
         printf("Element at position (%d,%d): ", i, i);
         scanf("%d", &x);
         set(&m, i, i, x);
     }
     printf("The matrix is: \n");
-    display(m, n, n);
-    
+    display(m);
+
     return 0;
 }
